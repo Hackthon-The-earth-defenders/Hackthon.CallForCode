a763365d610e71088b932e2f19a99718dd38981b
Leonardo Dorathoto <1705323@aluno.univesp.br>
2020-07-27 20:18:06 -0300

    inicial

---
+++ Hackthon/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hackthon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
