6ab9125ff70cb7105c7db850489c458bbf8ca37f
Leonardo Dorathoto <1705323@aluno.univesp.br>
2020-07-29 18:40:43 -0300

    feat: textos padronizados para validação de models

--- Hackthon/Models/Usuario.cs
+++ Hackthon/Models/Usuario.cs
@@ -1,4 +1,5 @@
 ﻿using Hackthon.Models.Enums;
+using Hackthon.Resources;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -16,16 +17,25 @@ namespace Hackthon.Models
     public class Usuario : IdentityUser<Guid>
     {
         [Display(Name = "Nome Completo")]
+        [MaxLength(200, ErrorMessageResourceType = typeof(TextosValidacao), ErrorMessageResourceName = nameof(TextosValidacao.MaxLength))]
+        [Required(ErrorMessageResourceType = typeof(TextosValidacao), ErrorMessageResourceName = nameof(TextosValidacao.Required))]
         public string Nome { get; set; }
 
+        [Required(ErrorMessageResourceType = typeof(TextosValidacao), ErrorMessageResourceName = nameof(TextosValidacao.Required))]
+        [Display(Name = "Tipo de Cadastro")]
         public TipoUsuario TipoCadastro { get; set; }
 
+        [MaxLength(200, ErrorMessageResourceType = typeof(TextosValidacao), ErrorMessageResourceName = nameof(TextosValidacao.MaxLength))]
+        [Display(Name = "Nome fantasia")]
         public string NomeFantasia { get; set; }
 
+        [MaxLength(20, ErrorMessageResourceType = typeof(TextosValidacao), ErrorMessageResourceName = nameof(TextosValidacao.MaxLength))]
+        [Display(Name = "CPF ou CNPJ")]
         //se TipoCadastro=usuario então CPF
         public string CpfCnpj { get; set; }
 
         [MaxLength(200, ErrorMessageResourceType = typeof(TextosValidacao), ErrorMessageResourceName = nameof(TextosValidacao.MaxLength))]
+        [Display(Name = "Atividade Economica")]
         public string AtividadeEconomica { get; set; }
 
     }
